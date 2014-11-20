a32a932a471874e18518bbd72c688647584287de
Darrell Lewis <darrelllewis@Darrells-MacBook-Pro.local>
2014-11-19 21:06:23 -0700

    Initial Commit

---
+++ WhereIs.IOS/Pods/Target Support Files/Pods-WhereIs.IOS-AFNetworking/Pods-WhereIs.IOS-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WhereIs_IOS_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_WhereIs_IOS_AFNetworking
+@end
---
+++ WhereIs.IOS/WhereIs.IOSTests/WhereIs_IOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  WhereIs_IOSTests.swift
+//  WhereIs.IOSTests
+//
+//  Created by Darrell Lewis on 11/19/14.
+//  Copyright (c) 2014 WM. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WhereIs_IOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
